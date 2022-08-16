266d7fed86d7d7f88539dd7d366f372b8d849462
meoflaxz <meorhashim@gmail.com>
2022-08-16 23:57:40 +0800

    feat: null and undefined

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,2 @@
-let ninjas = ["shaun", "ryu", "chun-li"];
-console.log(ninjas[1]);
-
-let ages = [20, 25, 30, 35];
-console.log(ages[2]);
-
-let random = ["shaun", "crystal", 30, 20];
-console.log(random);
-
-console.log(random.length);
-
-let test = ninjas.join(".");
-console.log(test);
-
-let tes = ninjas.concat(["ken", "crystal"]);
-console.log(tes);
-
-//return length of new array
-//alters original value
-let set = ninjas.push("ken");
-console.log(set);
-
-//remove and return last element of array
-let reg = ninjas.pop();
-console.log(reg);
+let age = null;
+console.log(age, age + 3, "the age is ${age}");
