0e67488995a41962fcaa5cb23c212628577ff64d
meoflaxz <meorhashim@gmail.com>
2022-08-16 23:47:27 +0800

    feat: arrays

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,25 @@
-const title = "Best reads of 2019";
-const author = "Mario";
-const likes = 30;
+let ninjas = ["shaun", "ryu", "chun-li"];
+console.log(ninjas[1]);
 
-let result = `The blog called ${title} by ${author} has ${likes} likes`;
-console.log(result);
+let ages = [20, 25, 30, 35];
+console.log(ages[2]);
 
-//creating html templates
-let html = `
-    <h2>${title}</h2>
-    <p>By ${author}</p>
-    <span>This blog has ${likes} likes </span>
-    `;
-console.log(html);
+let random = ["shaun", "crystal", 30, 20];
+console.log(random);
+
+console.log(random.length);
+
+let test = ninjas.join(".");
+console.log(test);
+
+let tes = ninjas.concat(["ken", "crystal"]);
+console.log(tes);
+
+//return length of new array
+//alters original value
+let set = ninjas.push("ken");
+console.log(set);
+
+//remove and return last element of array
+let reg = ninjas.pop();
+console.log(reg);
