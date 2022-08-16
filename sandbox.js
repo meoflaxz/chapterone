f5f5119321e36e418690a75ed9fd8977b5a25c40
meoflaxz <meorhashim@gmail.com>
2022-08-17 00:06:08 +0800

    feat: boolean and comparison

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,24 @@
-let age = null;
-console.log(age, age + 3, "the age is ${age}");
+console.log(true, false);
+
+let email = "test@gmail.com";
+let names = ["mario", "luigi", "toad"];
+
+//let result = email.includes("@");
+let result = names.includes("bowser");
+
+console.log(result);
+
+//comparison operators
+/*let age = 25;
+console.log(age == 25);
+console.log(age != 25);
+console.log(age < 20);
+*/
+
+let name = "shaun";
+console.log(name == "shaun");
+console.log(name == "Shaun");
+console.log(name > "crystal");
+console.log(name > "Shaun");
+
+//lower case are greater than upper case letter
