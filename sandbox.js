2dbd623883504e04a7925979382323b360dca5a9
meoflaxz <meorhashim@gmail.com>
2022-08-16 15:35:35 +0800

    feat : string method

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,7 @@
-console.log(1);
-console.log(2);
+let email = "test@gmailt.com";
+let result = email.lastIndexOf("t");
+
+console.log(email.slice(2, 6));
+console.log(email.substr(2, 6));
+
+console.log(email.replace("m", "w"));
