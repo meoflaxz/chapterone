22aebe22932564a0971cd9f35c7a753d86fe8ae8
meoflaxz <meorhashim@gmail.com>
2022-08-18 21:58:24 +0800

    feat: while loop

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,10 @@
-// for loops
+// while loops
 
-// for (let i = 0; i < 5; i++) {
-//   console.log(i);
-// }
-// console.log("finished loop");
+const names = ["mario", "luigi", "shaun"];
 
-const names = ["shaun", "mario", "luigi"];
+let i = 0;
 
-for (let i = 0; i < names.length; i++) {
-  //   console.log(names[i]);
-  let html = `<div>${names[i]}</div>`;
-  console.log(html);
+while (i < names.length) {
+  console.log("in loop: ", `<div>${names[i]}</div>`);
+  i++;
 }
